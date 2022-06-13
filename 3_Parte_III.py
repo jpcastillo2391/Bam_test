00cafe039f9db6d3eac1016975715fe4738cf9e9
Jose Castillo rodas <josecastillo.uvg@gmail.com>
2022-06-13 01:39:23 -0600

    p

---
+++ 3_Parte_III.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 13 00:56:52 2022
+
+Testing csv to postgres on pandas
+Created by Jose Castillo
+Github  https://github.com/jpcastillo2391
+
+"""
+#mi databricks ejecuta spark 3.2.1
+#pip install -U "databricks-connect==7.3.*4"  
